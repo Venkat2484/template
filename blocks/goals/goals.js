001a068107437559b92978a549fba82bcdc46daa
Venkat2484 <Venkatesh.Prasad@cognizant.com>
2026-03-11 16:37:09 +0530

    Update goals.js

--- blocks/goals/goals.js
+++ blocks/goals/goals.js
@@ -1,1 +1,15 @@
 
+export default function decorate(block) {
+
+  // Mark first two as you originally needed
+
+  const first = block.children[0];
+
+  if (!first) return;
+
+  first.classList.add('nav-head');
+ 
+  const second = block.children[1];
+
+  if (second) second.classList.add('nav-head-2');
+}
