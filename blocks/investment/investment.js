804a9de392327e1db66bdeb70c521b995eb4a5ef
Venkat2484 <Venkatesh.Prasad@cognizant.com>
2026-03-12 11:30:14 +0530

    Update investment.js

--- blocks/investment/investment.js
+++ blocks/investment/investment.js
@@ -1,13 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
  
-const wrapper = document.querySelector(".investment.block");
+  const divs = document.querySelectorAll(".investment.block > div");
  
-const divs = wrapper.querySelectorAll(":scope > div");
- 
-divs[0].classList.add("investment-header");
-divs[1].classList.add("investment-item-1");
-divs[2].classList.add("investment-item-2");
-divs[3].classList.add("investment-item-3");
-divs[4].classList.add("investment-item-4");
+  divs[0].classList.add("investment-head");
+  divs[1].classList.add("investment-item1");
+  divs[2].classList.add("investment-item2");
+  divs[3].classList.add("investment-item3");
+  divs[4].classList.add("investment-item4");
  
 });
