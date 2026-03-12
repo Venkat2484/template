69dacf4548748202dcf807d5ef2ed5d46898305d
Venkat2484 <Venkatesh.Prasad@cognizant.com>
2026-03-12 11:44:04 +0530

    Update investment.js

--- blocks/investment/investment.js
+++ blocks/investment/investment.js
@@ -6,8 +6,15 @@ export default function decorate(block) {
   /* main class */
   container.classList.add("nav-head");
  
-  /* create track */
-  const track = document.createElement("div");
-  track.className = "nav-head-2";
+  /* child elements */
+  const child1 = block.children[1];
+  const child2 = block.children[2];
+  const child3 = block.children[3];
+  const child4 = block.children[4];
+ 
+  if (child1) child1.classList.add("investment-item1");
+  if (child2) child2.classList.add("investment-item2");
+  if (child3) child3.classList.add("investment-item3");
+  if (child4) child4.classList.add("investment-item4");
  
 }
