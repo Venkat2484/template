eff7e2eb4c3a068f7e010eee7a54c9a1c8f5d515
Venkat2484 <Venkatesh.Prasad@cognizant.com>
2026-03-12 10:16:21 +0530

    Update investment.js

--- blocks/goals/investment.js
+++ blocks/goals/investment.js
@@ -1,18 +1,13 @@
-
 export default function decorate(block) {
-  const wrapper = block.children[0];
-  if (!wrapper) return;
-
-  wrapper.classList.add('investment-head');
-
-  // Expecting 4 divs inside wrapper
-  const div1 = wrapper.children[0];
-  const div2 = wrapper.children[1];
-  const div3 = wrapper.children[2];
-  const div4 = wrapper.children[3];
-
-  if (div1) div1.classList.add('goals-title');
-  if (div2) div2.classList.add('goals-description');
-  if (div3) div3.classList.add('goals-extra');        // custom class 3
-  if (div4) div4.classList.add('goals-footer');       // custom class 4
+ 
+  const container = block.children[0];
+  if (!container) return;
+ 
+  /* main class */
+  container.classList.add("nav-head");
+ 
+  /* create track */
+  const track = document.createElement("div");
+  track.className = "nav-head-2";
+ 
 }
