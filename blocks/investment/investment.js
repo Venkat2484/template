15183446f68d2eedc424aa10e078077f0d14f8af
Venkat2484 <Venkatesh.Prasad@cognizant.com>
2026-03-12 12:04:59 +0530

    Update investment.js

--- blocks/investment/investment.js
+++ blocks/investment/investment.js
@@ -2,25 +2,14 @@ export default function decorate(block) {
  
   const rows = [...block.children];
  
-  const image = rows[0];
-  const title = rows[1];
-  const item1 = rows[2];
-  const item2 = rows[3];
-  const item3 = rows[4];
+  /* image */
+  if (rows[0]) rows[0].classList.add("nav-head");
  
-  image.classList.add("nav-head");
-  title.classList.add("investment-title");
+  /* title section */
+  if (rows[1]) rows[1].classList.add("investment-title");
  
-  item1.classList.add("investment-item");
-  item2.classList.add("investment-item");
-  item3.classList.add("investment-item");
- 
-  /* create wrapper for items */
-  const itemsWrapper = document.createElement("div");
-  itemsWrapper.className = "investment-items";
- 
-  itemsWrapper.append(item1, item2, item3);
- 
-  block.append(itemsWrapper);
+  /* item groups */
+  if (rows[2]) rows[2].classList.add("investment-item");
+  if (rows[3]) rows[3].classList.add("investment-item");
  
 }
