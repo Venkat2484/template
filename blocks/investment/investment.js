8271c597ddc3aa459c0f7d3291e498e24a2a205a
Venkat2484 <Venkatesh.Prasad@cognizant.com>
2026-03-12 12:19:28 +0530

    Update investment.js

--- blocks/investment/investment.js
+++ blocks/investment/investment.js
@@ -5,19 +5,5 @@ export default function decorate(block) {
   const item2 = block.querySelector('.investment-item2');
   const item3 = block.querySelector('.investment-item3');
  
-  /* create right side wrapper */
-  const right = document.createElement('div');
-  right.className = 'investment-right';
- 
-  /* create grid wrapper */
-  const grid = document.createElement('div');
-  grid.className = 'investment-grid';
- 
-  /* move items */
-  grid.append(item1, item2, item3);
-  right.append(grid);
- 
-  /* add to block */
-  block.append(right);
  
 }
